fb59865caf2231a6efcdaa55edc5f3d2690296ed
unknown <mikhaylov.vlaadislav@yandex.ru>
2024-07-14 21:11:38 +0300

    add interceptors for status code 401 and refresh request

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,5 +3,30 @@ import * as settings from '../settings'
 
 
 export const instance = axios.create({
-    baseURL: settings.BASE_URL,
+  baseURL: settings.BASE_URL,
 })
+
+
+instance.interceptors.response.use(function (response) {
+  return response;
+}, async function (error) {
+  const originalRequest = error.config;
+  if (error.response.status == 401 && error.config && !error.config._isRetry) {
+    originalRequest._retry = true;
+    try {
+      const deviceIdFromStorage = localStorage.getItem('deviceId');
+      await instance.post(`/auth/refresh`, null,
+        {
+          withCredentials: true,
+          params: {
+            deviceId: deviceIdFromStorage
+          }
+        })
+      return instance.request(originalRequest);
+    } catch (e) {
+      console.log('Not authorized')
+    }
+    return Promise.reject(error);
+  }
+  return Promise.reject(error);
+});
